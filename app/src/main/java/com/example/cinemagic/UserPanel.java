8f7c3b8200aa1522fa9d7a9fdb93f1b0a62b6435
SasithaLakshan <74831645+SasithaLakshan@users.noreply.github.com>
2022-05-11 04:35:56 +0530

    Booking Management

--- app/src/main/java/com/example/cinemagic/UserPanel.java
+++ app/src/main/java/com/example/cinemagic/UserPanel.java
@@ -17,52 +17,52 @@ public class UserPanel extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_panel);
 
-//        movieMgmt = (ImageButton) findViewById(R.id.movie_mgmt);
-//        userMgmt = (ImageButton) findViewById(R.id.user_mgmt);
-//        paymentMgmt = (ImageButton) findViewById(R.id.payment_mgmt);
-        bookingMgmt = (ImageButton) findViewById(R.id.booking_mgmt);
-//        log_out_btn = (ImageButton) findViewById(R.id.log_out);
+        movieMgmt = findViewById(R.id.movie_mgmt);
+        userMgmt = findViewById(R.id.user_mgmt);
+        paymentMgmt = findViewById(R.id.payment_mgmt);
+        bookingMgmt = findViewById(R.id.booking_mgmt);
+        log_out_btn = findViewById(R.id.log_out);
 
 
-//        movieMgmt.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Intent intent = new Intent(getApplicationContext(), movieManagement.class);
-//                startActivity(intent);
-//            }
-//        });
-//
-//        userMgmt.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Intent intent = new Intent(getApplicationContext(), userManagement.class);
-//                startActivity(intent);
-//            }
-//        });
-//
-//        paymentMgmt.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Intent intent = new Intent(getApplicationContext(), paymentManagement.class);
-//                startActivity(intent);
-//            }
-//        });
-//
-//        bookingMgmt.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Intent intent = new Intent(getApplicationContext(), CreateBooking.class);
-//                startActivity(intent);
-//            }
-//        });
+        movieMgmt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(UserPanel.this,MovieManagement.class );
+                startActivity(intent);
+            }
+        });
 
-//        log_out_btn.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Intent intent = new Intent(getApplicationContext(), UserLogin.class);
-//                startActivity(intent);
-//            }
-//        });
+        userMgmt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(UserPanel.this,UserManagement.class);
+                startActivity(intent);
+            }
+        });
+
+        paymentMgmt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(UserPanel.this,PaymentManagement.class);
+                startActivity(intent);
+            }
+        });
+
+        bookingMgmt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(UserPanel.this,BookingManagement.class);
+                startActivity(intent);
+            }
+        });
+
+        log_out_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Splash.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
