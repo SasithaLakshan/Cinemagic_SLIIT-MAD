93275abd684a52dc12066d61f2e425c1bc387ed9
virajddd <100112779+virajddd@users.noreply.github.com>
2022-05-11 07:46:59 +0530

    Ticket Management

---
+++ app/src/main/java/com/example/cinemagic/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.mad_2nd_year_2nd_semester;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button =findViewById(R.id.btn1);
+
+        button.setOnClickListener(new View.OnClickListener(){
+             @Override
+             public void onClick(View v){
+
+                Intent i=new Intent(MainActivity.this,EditUserProfilePage.class);
+                startActivity(i);
+
+             }
+
+
+        });
+
+
+    }
+}
